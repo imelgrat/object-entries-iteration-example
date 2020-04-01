bd31b55c9fef056ad1ab1d273e071127681152c9
Iván Melgrati <imelgrat@users.noreply.github.com>
2020-03-31 22:44:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ let car = { color: "blue", model: "Sandero", doors: 4 };
 const app = document.getElementById("app");
 
 // Create an array with the object's enumerable values
-const values = Object.values(car);
+const entries = Object.entries(car);
 app.insertAdjacentHTML(
   "beforeend",
-  "The car's property values are:  " + values.join(", ")
+  "The car's property entries are:  <br>";
 );
+for (let [key, value] of Object.entries(entries)) {
+  app.insertAdjacentHTML(
+    "beforeend",
+    `${key}: ${value}<br>`;
+  );
+}
